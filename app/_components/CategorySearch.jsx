72fa84f8924660581e7a1136da545f56a3cd5abc
RUTUKAPATE <rutuja16kapate@gmail.com>
2024-06-05 21:36:44 +0530

    heroPushed

---
+++ app/_components/CategorySearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CategorySearch() {
+  return (
+    <div className='mb-10'>
+        <h2>Search Doctors</h2>
+    </div>
+  )
+}
+
+export default CategorySearch
